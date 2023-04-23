3f6d9318a197a99cdc11aa4dd3b11e4a4ff20c25
Hannapham1007 <hannaphamm1007@gmail.com>
2023-04-24 00:37:18 +0200

    remove celebrate1 page

--- lib/celebrate.dart
+++ lib/celebrate.dart
@@ -1,51 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:fluttermaritime/main.dart';
-import 'package:fluttermaritime/theshipex.dart';
+import 'package:fluttermaritime/radiocompage.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Celebrate extends StatelessWidget {
-  const Celebrate({super.key});
+class Celebrate1 extends StatelessWidget {
+  const Celebrate1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        //backgroundColor: const Color.fromRGBO(235, 239, 245, 1.0),
+        backgroundColor: Color.fromRGBO(245, 247, 250, 1.0),
         body: Center(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'WELL DONE!',
-              style: Theme.of(context).textTheme.headline4,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'LESSON COMPLETED!',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Lottie.asset('assets/welldone.json', height: 270.h),
+                SizedBox(
+                  height: 50.h,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Radiocompage()),
+                          (route) => route.isFirst);
+                    },
+                    style: ButtonStyle(
+                        minimumSize:
+                            MaterialStateProperty.all<Size>(Size(280.w, 50.h)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.circular(20.r)))),
+                    child: Text(
+                      'BACK TO HOME',
+                      style: TextStyle(
+                          fontSize: 18.sp, fontWeight: FontWeight.bold),
+                    )),
+              ],
             ),
-            Lottie.asset('assets/welldone.json', height: 270.h),
-            SizedBox(
-              height: 50.h,
-            ),
-            /*ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ShipExercise()),
-                  );
-                },
-                style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(Size(280.w, 50.h)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.r)))),
-                child: Text(
-                  'CONTINUE',
-                  style:
-                      TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
-                )),*/
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
--- lib/theshipex.dart
+++ lib/theshipex.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttermaritime/celebrate1.dart';
+import 'package:fluttermaritime/celebrate.dart';
 import 'package:fluttermaritime/theshipdata.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_tts/flutter_tts.dart';
